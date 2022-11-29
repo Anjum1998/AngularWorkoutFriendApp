4de5b1028957d3b71833f4cdfb91797481e14424
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-29 21:47:36 +0530

    update

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchFriend=()=>
+  {
+    return this.http.get("https://dummyapifriends.herokuapp.com/view")
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewFriendComponent } from './view-friend/view-friend.component';
+import { HttpClientModule } from '@angular/common/http';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ViewFriendComponent
 
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewFriendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
